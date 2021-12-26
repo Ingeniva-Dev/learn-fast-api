7204a4ea86efd56006e566e66ac3ed4d96b1c670
Gemma Whitehouse <gemmawhitehouse@Gemmas-MacBook-Pro-2.local>
2021-12-26 18:21:35 +0000

    added post request and pydantic

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 
 @app.get("/")
 def root():
@@ -11,8 +20,9 @@ def root():
 def get_posts():
     return{"data": "This is your posts"}
 
-@app.post("/createpost")
-def create_posts(payload:dict = Body(...)):
-    print(payload)
-    return{"message": "succesfully created post"}
+@app.post("/posts")
+def create_posts(new_post: Post):
+    print(new_post.rating)
+    print(new_post.dict())
+    return{"data":"new post"} 
 
