7069123c2ed28516dd9f183511060679357c9531
Gemma Whitehouse <gemmawhitehouse@Gemmas-MacBook-Pro-2.local>
2022-01-19 18:55:51 +0000

    tried adding .env file and adding variables to hide pw etc - now cannot connect to DB

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from typing import Optional
+import dotenv
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = FastAPI()
 
@@ -10,7 +19,22 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+    # rating: Optional[int] = None
+
+USER="postgres"
+PASSWORD="XXXXXXXX"
+
+while True:
+
+    try:
+        conn = psycopg2.connect(host = 'localhost', database='fastapi', user=USER, password=PASSWORD)
+        cursor = conn.cursor()
+        print("Database connection was successful!")
+        break
+    except Exception as error:
+        print("connecting to DB failed")
+        print("Error: ", error)
+        time.sleep(2)
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favoriate foods", "content": "I like pizza", "id": 2}]
 
@@ -31,17 +55,25 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return{"data": my_posts}
+    cursor.execute("""SELECT * FROM posts """)
+    posts = cursor.fetchall()
+    # print(posts)
+    return{"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 10000000)
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+    
+            (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    # post_dict = post.dict()
+    # ost_dict['id'] = randrange(0, 10000000)
     # print(post)
     # print(post.dict())
+    conn.commit()
 
-    my_posts.append(post.dict)
-    return{"data": post_dict} 
+    # my_posts.append(post.dict)
+    return{"data": new_post} 
 
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response):
